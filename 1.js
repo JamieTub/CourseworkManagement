a2aa478f0d676e095ada7023e9a2e61aba1a2e54
steven <stevenkei98@gmail.com>
2020-04-10 19:57:59 +0100

    added to dbseed + cw-entry post form

--- 1.js
+++ 1.js
@@ -2,17 +2,19 @@
 var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
+bodyParser = require('body-parser'),
+app.use(bodyParser.urlencoded({extended: false}));
 
 //mustache
 mustache = require('mustache-express'),
 path = require('path');
 app.engine('mustache', mustache());
 app.set('view engine', 'mustache');
-//app.set('views', path.resolve(__dirname, 'mustache'));
 
 //db
 const Seed = require("./dbSeed");
 let seed = new Seed();
+seed.init();
 
 //seed.initUser();
 app.get('/', function(req, res){
@@ -27,43 +29,37 @@ app.get('/about', function(req, res){
     res.send('About');
 });
 
-//coursework page
-app.get("/coursework", function(request, response){
-    response.render("coursework", {
-        'entries' : [
-            {
-                'Title' : 'Chemistry',
-                'Module' : 'Chemistry PT',
-                'DueDate' : '07/08/2020',
-                'CompDate' : '05/04/2020'
-            },
-            {
-                'Title' : 'History',
-                'Module' : 'History 1912 WWI',
-                'DueDate' : '07/04/2020',
-                'CompDate' : '05/04/2020'
-            },
-            {
-                'Title' : 'Physics',
-                'Module' : 'Physics LG',
-                'DueDate' : '07/06/2020',
-                'CompDate' : '10/06/2020'
-            },
-            {
-                'Title' : 'Mathematics',
-                'Module' : 'Mathematics QE',
-                'DueDate' : '06/06/2020',
-                'CompDate' : '02/03/2020'
-            },
-            {
-                'Title' : 'Gym',
-                'Module' : 'GYM LDR',
-                'DueDate' : '02/05/2020',
-                'CompDate' : '02/05/2020'
-            }
-        ]
-    });
+app.get("/coursework", function (request, response) {
+    seed.all()
+        .then((list) => {
+            console.log(list);
+            response.render("coursework", {
+                entries: list
+            });
+        })
+        .catch((err) => {
+            console.log('Error: ')
+            console.log(JSON.stringify(err))
+        });
 });
+
+//new coursework
+app.get('/cw-entry', function(request, response){
+    response.render("cw-entry");
+})
+
+//cw-entry post
+app.post('/post', function(request, response){
+    if (!request.body.User || !request.body.Title) {
+        response.status(400).send("Coursework Entries must have a title and user.");
+        return;
+    }
+    
+entries.create(request.body.Title, 
+request.body.Module,request.body.DueDate, request.body.CompDate);                          
+    response.redirect("/coursework");
+})
+
 
 app.post('/register', function(req, res){
     res.type('text');
@@ -85,6 +81,8 @@ app.use(function (err, req, res, next) {
     res.status(500);
     res.send('500 - Server Error');
 });
+
+
 app.listen(app.get('port'), function () {
 console.log('Express started on http://localhost:' + 
 app.get('port') + '; press Ctrl-C to terminate.'
--- dbSeed.js
+++ dbSeed.js
@@ -12,11 +12,88 @@ james = {
 
   class Seed{
      
-    constructor(){}
+    constructor(dbFilePath) {
+      //run db as a file
+      if (dbFilePath) {
+          this.db = new Datastore({ filename: dbFilePath, autoload: true });
+          console.log("DB connected to file: " + dbFilePath);
+      } else {
+          //in memory
+          this.db = new Datastore();
+      }
+    }
 
     initUser(){
         Users.insert(james, function(err, User){console.log(User)})
     }
+    
+    all() {
+      return new Promise((resolve, reject) => {
+          this.db.find({}, function(err, entries) {
+              if (err) {
+                  reject(err);
+                  console.log('rejected');
+              } else {
+                  resolve(entries);
+                  console.log('resolved');
+              }
+
+          });
+      })
+  }
+
+   create(Title,Module,DueDate,CompDate) {
+      var entry = {
+        Title: Title,
+        Module: Module,
+        DueDate: DueDate,
+        CompDate: CompDate
+      };
+
+      this.db.insert(entry, function(err, doc){
+        if(err){
+          console.log("Can't insert entry title.: ", title);
+        }
+      });
+    }
+
+    init() {
+      this.db.insert({
+        Title : 'Chemistry',
+        Module : 'Chemistry PT',
+        DueDate : '07/08/2020',
+        CompDate : '05/04/2020'
+      });
+
+      this.db.insert({
+        Title : 'History',
+        Module : 'History 1912 WWI',
+        DueDate : '07/04/2020',
+        CompDate : '05/04/2020'
+      });
+
+      this.db.insert({
+        Title : 'Physics',
+        Module : 'Physics LG',
+        DueDate : '07/06/2020',
+        CompDate : '10/06/2020'
+      });
+
+      this.db.insert({
+        Title : 'Mathematics',
+        Module : 'Mathematics QE',
+        DueDate : '06/06/2020',
+        CompDate : '02/03/2020'
+      });
+
+      this.db.insert({
+        Title : 'Gym',
+        Module : 'GYM LDR',
+        DueDate : '02/05/2020',
+        CompDate : '02/05/2020'
+      });
+      
+    };
 
   }
 
