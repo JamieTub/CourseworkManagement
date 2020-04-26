d887b6d7dcc0cb803d45405a3706ae55547db95d
James Lawn <jl1888@live.co.uk>
2020-04-26 15:02:28 +0100

    Adding user class for registration

---
+++ models/User.js
@@ -0,0 +1,12 @@
+class User {
+    constructor(){
+        this.firstName = "";
+        this.lastName = "";
+        this.email = "";
+        this.password = "";
+    }
+
+    getName(){
+        return this.foreName + " " + this.surName;
+    }
+}
